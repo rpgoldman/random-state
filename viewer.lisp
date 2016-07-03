ba15c865a1a0058cc79445c761c8688552a47891
Shinmera <shinmera@tymoon.eu>
2016-07-03 17:17:08 +0200

    Move the showing of the time into a status bar widget

--- viewer.lisp
+++ viewer.lisp
@@ -44,8 +44,7 @@
     a))
 
 (defmethod generate ((viewer viewer) (generator random-state::generator))
-  (let ((buffer (buffer viewer))
-        (start (get-internal-real-time)))
+  (let ((buffer (buffer viewer)))
     (unless (and buffer
                  (= (q+:width buffer) (q+:width viewer))
                  (= (q+:height buffer) (q+:height viewer)))
@@ -54,11 +53,9 @@
       (setf buffer (buffer viewer)))
     (dotimes (y (q+:height buffer))
       (dotimes (x (q+:width buffer))
-        (setf (q+:pixel buffer x y) (random-color generator))))
-    (format T "~&Generation took ~fs~%" (/ (- (get-internal-real-time) start)
-                                           internal-time-units-per-second))))
+        (setf (q+:pixel buffer x y) (random-color generator))))))
 
-(define-widget main (QWidget)
+(define-widget main (QMainWindow)
   ())
 
 (define-subwidget (main viewer) (make-instance 'viewer))
@@ -72,13 +69,19 @@
   (setf (q+:maximum seed) #xFFFFFFF)
   (setf (q+:value seed) (mod (get-universal-time) #xFFFFFFF)))
 
-(define-subwidget (main layout) (q+:make-qvboxlayout main)
-  (q+:add-widget layout viewer)
-  (let ((inner (q+:make-qhboxlayout)))
-    (q+:add-widget inner chooser)
-    (q+:add-widget inner seed)
-    (q+:add-widget inner regen)
-    (q+:add-layout layout inner)))
+(define-subwidget (main layout) (q+:make-qwidget main)
+  (let ((layout (q+:make-qvboxlayout layout)))
+    (q+:add-widget layout viewer)
+    (let ((inner (q+:make-qhboxlayout)))
+      (q+:add-widget inner chooser)
+      (q+:add-widget inner seed)
+      (q+:add-widget inner regen)
+      (q+:add-layout layout inner)))
+  (setf (q+:central-widget main) layout))
+
+(define-subwidget (main status) (q+:make-qlabel viewer)
+  (setf (q+:text status) "Ready.")
+  (q+:add-permanent-widget (q+:status-bar main) status))
 
 (define-slot (main regen) ()
   (declare (connected regen (clicked)))
@@ -86,9 +89,13 @@
         (seed (value seed)))
     (bt:make-thread
      (lambda ()
-       (setf (q+:enabled regen) NIL)
-       (generate viewer (random-state::make-generator type seed))
-       (setf (q+:enabled regen) T))
+       (let ((start (get-internal-real-time)))
+         (setf (q+:text status) "Generating...")
+         (setf (q+:enabled regen) NIL)
+         (generate viewer (random-state::make-generator type seed))
+         (setf (q+:enabled regen) T)
+         (setf (q+:text status) (format NIL "Generation took ~fs." (/ (- (get-internal-real-time) start)
+                                                                      internal-time-units-per-second)))))
      :initial-bindings `((*standard-output* . ,*standard-output*)))))
 
 (defun main ()
