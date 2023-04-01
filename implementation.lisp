833bfdcad215a7832bafaad9bae1930b06b9f47e
rpgoldman <rpgoldman@sift.net>
2023-04-01 05:27:38 -0500

    Fix RESEED method for RANDOM-STATE on Allegro and Clozure. (#23)
    * Fix RESEED method for RANDOM-STATE on Allegro and Clozure.
    
    ---------
    
    Co-authored-by: Shinmera <shinmera@tymoon.eu>

--- implementation.lisp
+++ implementation.lisp
@@ -18,13 +18,15 @@
   (error "Can't retrieve seed from RANDOM-STATE objects on ~a" (lisp-implementation-type)))
 
 (defmethod reseed ((generator random-state) seed)
-  #+sbcl ;; Yikes lol.
+  #+sbcl
   (let ((seeded (sb-ext:seed-random-state seed)))
     (replace (sb-kernel::random-state-state generator)
              (sb-kernel::random-state-state seeded)))
-  #-(or sbcl) #- (or sbcl)
-  (declare (ignorable generator) (ignore seed))
-  (error "Can't reseed RANDOM-STATE objects on ~a" (lisp-implementation-type)))
+  #+allegro
+  (setf (excl::random-state-seed generator) seed)
+  #-(or allegro sbcl)
+  (warn "Can't reseed RANDOM-STATE objects on ~a" (lisp-implementation-type))
+  generator)
 
 (defmethod next-byte ((generator random-state))
   (cl:random most-positive-fixnum generator))
