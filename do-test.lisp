2ff9f39209b70561f1b1c76b3715386ef956ef50
Robert P. Goldman <rpgoldman@sift.net>
2023-04-01 15:17:40 -0500

    Add portable test script settting exit status on failure.

---
+++ do-test.lisp
@@ -0,0 +1,34 @@
+#|
+ This file is a part of random-state
+ (c) 2023 Robert P. Goldman and SIFT, LLC
+ Author: Robert P. Goldman <rpgoldman@sift.net>
+|#
+
+
+(in-package :common-lisp-user)
+
+(defpackage test-random-state)
+
+(load (merge-pathnames (make-pathname :directory '(:relative "quicklisp") :name "setup" :type "lisp")
+                       (user-homedir-pathname)))
+
+(push (namestring (uiop:pathname-directory-pathname *load-truename*)) ql:*local-project-directories*)
+
+(assert (uiop:pathname-equal (asdf:component-pathname (asdf:find-system "random-state"))
+                             (uiop:pathname-directory-pathname *load-truename*)))
+
+(ql:quickload "random-state-test")
+
+(define-condition tests-failed (error)
+  ()
+  (:documentation "Include this condition so that we can detect failure of the test-op.")
+  )
+
+(defmethod parachute:summary :after ((report parachute:report))
+  (call-next-method)
+  (when (eq (parachute:status report) :failed)
+    (signal 'tests-failed)))
+
+(handler-case
+    (asdf:test-system "random-state")
+  (tests-failed ()  (uiop:quit 1)))
