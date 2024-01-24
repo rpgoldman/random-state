ea878d411dfa6f2ae4737ddad9a8a78105e9757c
Shinmera <shinmera@tymoon.eu>
2024-01-24 20:52:06 +0100

    Fix quasi

--- quasi.lisp
+++ quasi.lisp
@@ -1,12 +1,13 @@
 (in-package #:org.shirakumo.random-state)
 
 (define-generator quasi 'single-float (stateful-generator)
-    ((source *generator* :type generator)
+    ((source *generator* :type T)
      (last 0.0 :type single-float))
   (:reseed
    (reseed source seed)
    (setf last 0.0))
   (:next
+   (declare (optimize speed (safety 1)))
    (setf last (mod (+ last 0.5 (random-unit source)) 1.0))))
 
 ;; TODO: multivariate
