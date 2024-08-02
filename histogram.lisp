10090409138e778a223a2ec9e0e687df79394793
Robert Goldman <rpgoldman@sift.net>
2024-08-02 16:43:40 -0500

    Move benchmark functions.
    These make reference to stuff that is defined in generator.lisp, which
    is loaded downstream.  This causes CCL heartburn.

--- histogram.lisp
+++ histogram.lisp
@@ -36,3 +36,33 @@
              (incf total (abs deviation)))
     (format stream "Cumulative deviation: ~6,3f%~%" total)
     histogram))
+
+
+(defun benchmark (rng &key (samples 1000000) (stream *standard-output*))
+  ;; this declaration is necessary because ENSURE-GENERATOR is a
+  ;; forward-reference and SBCL does not like it that it misses the
+  ;; chance to apply the compiler macro here. [2024/07/29:rpg]
+  (let* ((rng (ensure-generator rng))
+         (next-fun (next-byte-fun rng))
+         (start (get-internal-run-time)))
+    (declare (type (unsigned-byte 64) samples))
+    (declare (type (function (generator) T) next-fun))
+    (locally (declare (optimize speed (safety 0)))
+      (loop repeat samples
+            do (funcall next-fun rng)))
+    (let* ((end (get-internal-run-time))
+           (duration (float (/ (- end start) INTERNAL-TIME-UNITS-PER-SECOND) 0d0)))
+      (format stream "Duration: ~12t~10,1f~%Samples: ~12t~10d~%Samples/s: ~12t~10,1f~%S/sample: ~12t~10,8f"
+              duration samples (/ samples duration) (/ duration samples))
+      (/ samples duration))))
+
+(defun benchmark-all (&key (samples 1000000) (stream *standard-output*))
+  (let* ((nullstream (make-broadcast-stream))
+         (stats (loop for rng in (list-generator-types)
+                      for result = (cons rng (handler-case (benchmark rng :samples samples :stream nullstream)
+                                               (error () -1)))
+                      when result collect result)))
+    (setf stats (sort stats #'> :key #'cdr))
+    (format stream "RNG~20tSamples/second~%")
+    (loop for (rng . samples) in stats
+          do (format stream "~&~a~20t~18,1f~%" rng samples))))
